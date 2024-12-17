3e42200a5be01d266b2e43b111e2cd5305c1a172
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-12-17 20:53:47 +0530

    add lint config, & lint the project

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import onlyWarn from "eslint-plugin-only-warn";
 
 /**
@@ -16,9 +18,23 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unused-imports/no-unused-imports": "error",
+
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   },
   {
