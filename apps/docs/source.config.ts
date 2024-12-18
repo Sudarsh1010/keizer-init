ec0e6b5576e70381019884e87d90131893e62492
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-12-19 02:45:44 +0530

    fix docs

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import { defineCollections, metaSchema } from "fumadocs-mdx/config";
 
-export const { docs, meta } = defineDocs({
+export const docs = defineCollections({
+  type: "doc",
   dir: "content/docs",
 });
 
-export default defineConfig();
+export const meta = defineCollections({
+  type: "meta",
+  dir: "content/docs",
+  schema: metaSchema,
+});
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,14 +1,14 @@
-import * as config from "@repo/ui/tailwind.config";
 import { createPreset } from "fumadocs-ui/tailwind-plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  ...config,
-  content: [
-    ...config.content,
-    "./content/**/*.{md,mdx}",
-    "./mdx-components.{ts,tsx}",
-    "./node_modules/fumadocs-ui/dist/**/*.js",
-  ],
+  darkMode: "class",
   presets: [createPreset()],
+  content: [
+    "./node_modules/fumadocs-ui/dist/**/*.js",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./content/**/*.mdx",
+    "./mdx-components.tsx",
+  ],
 };
